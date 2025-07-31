723a15261d2957461195b939231a1c3c9aa1d2e4
erbarraud <154258392+erbarraud@users.noreply.github.com>
2025-07-31 13:02:59 +0200

    Publish application

--- dist/assets/OrderDetails-B6-bLD6A.js
+++ dist/assets/OrderDetails-B6-bLD6A.js
@@ -1,4 +1,4 @@
-import{c as k,r as y,o as j,u as T,C,a as V,T as M,b as n,d as _,e as t,f as s,_ as x,F as b,g as h,h as i,i as B,t as o,w as a,j as c,k as w,l as N,m,n as $,p as d,q as u,A as R,D as F,S as z,P as I,s as v,v as J,x as E,y as L,z as D,B as G,E as H,G as K,H as q,I as Q,J as U,K as W}from"./index-CuUQQHn5.js";/**
+import{c as k,r as y,o as j,u as T,C,a as V,T as M,b as n,d as _,e as t,f as s,_ as x,F as b,g as h,h as i,i as B,t as o,w as a,j as c,k as w,l as N,m,n as $,p as d,q as u,A as R,D as F,S as z,P as I,s as v,v as J,x as E,y as L,z as D,B as G,E as H,G as K,H as q,I as Q,J as U,K as W}from"./index-BQCKI2ow.js";/**
  * @license lucide-vue-next v0.525.0 - ISC
  *
  * This source code is licensed under the ISC license.
